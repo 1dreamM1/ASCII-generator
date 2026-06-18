1eb78d8224dc07d1de9cefc312682bb773a5b853
dreamM <538612441dreamM1@users.noreply.github.com>
2026-06-18 21:17:49 +0300

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -11,3 +12,18 @@ export default {
     require('tailwindcss-animate'),
   ],
 }
+=======
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('tailwindcss-animate'),
+  ],
+}
+>>>>>>> 84254e0 (Initial commit)
