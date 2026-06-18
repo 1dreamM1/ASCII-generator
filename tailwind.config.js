b58bc8408d2b967b277733533e301f02dbd63de4
dreamM <538612441dreamM1@users.noreply.github.com>
2026-06-18 21:30:59 +0300

    Fix merge conflict leftovers

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -12,18 +12,3 @@ export default {
     require('tailwindcss-animate'),
   ],
 }
-=======
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('tailwindcss-animate'),
-  ],
-}
->>>>>>> 84254e0 (Initial commit)
